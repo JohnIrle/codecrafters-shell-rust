f908a0955a673ca3cd8ff96ae439afe28a10ad9f
John Irle <irlejohn@gmail.com>
2024-05-24 16:05:02 -0500

    break commands into enum and parse path

---
+++ src/command.rs
@@ -0,0 +1,79 @@
+use std::path::PathBuf;
+use std::str::SplitWhitespace;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Command<'a> {
+    Builtin(Builtin),
+    Executable(String),
+    NotFound(&'a str),
+}
+
+impl<'a> From<&'a str> for Command<'a> {
+    fn from(value: &'a str) -> Self {
+        if let Ok(builtin) = Builtin::try_from(value) {
+            return Self::Builtin(builtin);
+        }
+
+        if let Some(executable) = std::env::var("PATH")
+            .expect("PATH not set")
+            .split(':')
+            .map(PathBuf::from)
+            .find_map(|path| {
+                let executable = path.join(value);
+                if executable.exists() && executable.is_file() {
+                    Some(executable)
+                } else {
+                    None
+                }
+            })
+        {
+            return Self::Executable(executable.to_string_lossy().to_string());
+        }
+
+        Self::NotFound(value)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Builtin {
+    Exit,
+    Echo,
+    Type,
+}
+
+
+impl Builtin {
+    pub fn run(&self, mut args: SplitWhitespace) {
+        match self {
+            Self::Exit => {
+                std::process::exit(args.next().map_or(0, |code| {
+                    code.parse().expect("could not parse exit code to i32")
+                }))
+            }
+            Self::Echo => {
+                println!("{}", args.collect::<Vec<_>>().join(" "))
+            }
+            Self::Type => {
+                let command = args.next().expect("missing arg");
+                match Command::from(command) {
+                    Command::Builtin(_) => println!("{command} is a shell builtin"),
+                    Command::Executable(path) => println!("{command} is {path}"),
+                    Command::NotFound(_) => println!("{command}: not found"),
+                }
+            }
+        }
+    }
+}
+
+impl TryFrom<&str> for Builtin {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "exit" => Ok(Builtin::Exit),
+            "echo" => Ok(Builtin::Echo),
+            "type" => Ok(Builtin::Type),
+            _ => Err(())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[allow(unused_imports)]
+mod command;
+
 use std::io::{self, Write};
+use crate::command::Command;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
-    let builtin_commands = vec!["exit", "echo", "type"];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -14,33 +15,13 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         // Get command
-        let command = input.trim();
-        match command {
-            command if command.starts_with("exit") => {
-                match command.split_once(' ') {
-                    None => std::process::exit(0),
-                    Some((_, arg)) => std::process::exit(arg.parse().unwrap())
-                }
-            }
-            command if command.starts_with("echo") => {
-                match command.split_once(' ') {
-                    None => println!(),
-                    Some((_, arg)) => println!("{arg}")
-                }
-            }
-            command if command.starts_with("type") => {
-                match command.split_once(' ') {
-                    None => continue,
-                    Some((_, arg)) => {
-                        if builtin_commands.contains(&arg) {
-                            println!("{arg} is a shell builtin")
-                        } else {
-                            println!("{arg} not found")
-                        }
-                    }
-                }
-            }
-            command => println!("{command}: command not found")
+        let mut parts = input.split_whitespace();
+        let command = parts.next().expect("No command");
+        
+        match Command::from(command) {
+            Command::Builtin(builtin_command) => builtin_command.run(parts),
+            Command::Executable(_) => unimplemented!(),
+            Command::NotFound(command) => println!("{command}: command not found"), 
         }
     }
 }
