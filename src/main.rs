44f97a3f7744a724e7ecd9b94ac3c81fd5273df1
John Irle <irlejohn@gmail.com>
2024-05-24 14:42:23 -0500

    move command parsing into loop

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let command = input.trim();
-    println!("{command}: command not found");
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        // Get command
+        let command = input.trim();
+        println!("{command}: command not found");
+    }
 }
